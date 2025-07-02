c15a49f2aa54acd2862aa1c8f6dbdcea172e9ad4
AshrithaMadineni <ashrithamadineni552@gmail.com>
2025-07-02 16:08:07 +0530

    Add files via upload

---
+++ JS_samples/funexp2.js
@@ -0,0 +1,17 @@
+function findOddOrEven(no){
+    if(no%2==0){
+        console.log("even");
+    }else{
+        console.log("odd");
+    }
+}
+//using arrow function
+const oddOrEven=(no)=>{
+    if(no%2==0){
+        console.log("odd");
+    }else{
+
+    }
+}    
+findOddOrEven(17);
+oddOrEven(20);
---
+++ JS_samples/object.js
@@ -0,0 +1,3 @@
+let user={name:"ashritha",dep:"cse",age:20};
+console.log(user);
+console.log(user.name);
